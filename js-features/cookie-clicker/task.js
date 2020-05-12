04a0ce8f550c92a69e685768d189889a92cc954c
Ann Vasilyeva <skazkadreamanna@gmail.com>
2020-05-12 18:14:24 +0300

    добавлена задача провышенного уровня сложности

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,13 +1,20 @@
 const cookie1 = document.getElementById('cookie');
 
+let lastClick = Date.now();
+let speedCounter = document.getElementById('speed__counter');
+let clickerCounter = document.getElementById('clicker__counter');
 
 cookie1.onclick = function () {
+
     if (cookie1.width === 200) {
         cookie1.width = 220;
     } else {
         cookie1.width = 200;
     }
 
-    let clickerCounter = document.getElementById('clicker__counter');
     clickerCounter.textContent++;
+
+    speedCounter.textContent = (1000 / (Date.now() - lastClick)).toFixed(2);
+
+    lastClick = Date.now();
 };
