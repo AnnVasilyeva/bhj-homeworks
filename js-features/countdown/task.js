237f8b6d8dab0eaab18749d58683d03e0bcfcde0
Ann Vasilyeva <skazkadreamanna@gmail.com>
2020-05-12 11:19:17 +0300

    new branch: big_timer

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,13 +1,60 @@
-const getTimer = function () {
-    const timer = document.getElementById('timer');
+// const getTimer = function () {
+//     const timer = document.getElementById('timer');
+//
+//     if (timer.textContent > 0) {
+//         timer.textContent--;
+//     } else {
+//         clearInterval(timerId);
+//         return alert('Вы победили в конкурсе!');
+//     }
+// };
+//
+// let timerId = setInterval(getTimer, 1000);
 
-    if (timer.textContent > 0) {
-        timer.textContent--;
+
+const getTimer = function () {
+    let hours = document.getElementById('hours');
+    let minutes = document.getElementById('minutes');
+    let seconds = document.getElementById('seconds');
+
+
+    if (seconds.textContent > 0 && seconds.textContent > 10) {
+        seconds.textContent--;
+    } else if (seconds.textContent == 10) {
+        seconds.textContent = '0' + '9';
+    } else if (seconds.textContent < 10 && seconds.textContent[1] > 0) {
+        seconds.textContent = '0' + (seconds.textContent[1] -= 1);
     } else {
+        seconds.textContent = '59';
+    }
+
+    if (seconds.textContent == '59') {
+        if (minutes.textContent > 0 && minutes.textContent > 10) {
+            minutes.textContent--;
+        } else if (minutes.textContent == 10) {
+            minutes.textContent = '0' + '9';
+        } else if (minutes.textContent < 10 && minutes.textContent[1] > 0) {
+            minutes.textContent = '0' + (minutes.textContent[1] -= 1);
+        } else {
+            minutes.textContent = '59';
+        }
+    }
+
+    if (minutes.textContent == '59' && seconds.textContent == '59') {
+        if (hours.textContent > 0 && hours.textContent > 10) {
+            hours.textContent--;
+        } else if (hours.textContent == 10) {
+            hours.textContent = '0' + '9';
+        } else if (hours.textContent < 10 && hours.textContent[1] > 0) {
+            hours.textContent = '0' + (hours.textContent[1] -= 1);
+        }
+    }
+
+    if (seconds.textContent == '00' && minutes.textContent == '00' && hours.textContent == '00') {
         clearInterval(timerId);
         return alert('Вы победили в конкурсе!');
+
     }
-};
+}
 
 let timerId = setInterval(getTimer, 1000);
-
