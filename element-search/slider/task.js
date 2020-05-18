53e2db00b32defd7001da888a026f9f0d9c40be1
Ann Vasilyeva <skazkadreamanna@gmail.com>
2020-05-18 16:15:21 +0300

    task_3 changed

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -3,6 +3,7 @@ let sliderArrowNext = document.getElementsByClassName('slider__arrow_next');
 let sliderItem = document.querySelectorAll('.slider__item');
 let sliderDot = document.querySelectorAll('.slider__dot');
 
+// при клике на левую кнопку
 sliderArrowPrev[0].onclick = function () {
     let i = 0;
     let nextElement = sliderItem.length - 1;
@@ -19,6 +20,7 @@ sliderArrowPrev[0].onclick = function () {
     }
 };
 
+// при клике на правую кнопку
 sliderArrowNext[0].onclick = function () {
     let i = 0;
     let nextElement = 0;
@@ -35,6 +37,7 @@ sliderArrowNext[0].onclick = function () {
     }
 }
 
+// перебираем точки слайдера и при нажатии вызываем для каждой функцию setActive
 for (let i = 0; i < sliderDot.length; i++) {
     sliderDot[i].onclick = function () {
         setActive(i);
@@ -43,10 +46,12 @@ for (let i = 0; i < sliderDot.length; i++) {
 
 function setActive(active) {
     // убрать все активные элементы
-    for (let i = 0; i < sliderItem.length; i++) {
-        sliderItem[i].classList.remove('slider__item_active');
-        sliderDot[i].classList.remove('slider__dot_active');
-    }
+        //находим в массиве(переделываем из псевдомассива) sliderItem индекс элемента с активным классом
+    let index = [...sliderItem].findIndex((element) => element.classList.contains('slider__item_active'));
+        // у элементов с этим индексом убираем активный класс
+    sliderItem[index].classList.remove('slider__item_active');
+    sliderDot[index].classList.remove('slider__dot_active');
+
     //установить активную картинку
     sliderItem[active].classList.add('slider__item_active');
     //установить активную точку
