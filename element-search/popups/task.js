a8ae895330a71cd8f6bc7d4ea4caf515a0b0efdd
Ann Vasilyeva <skazkadreamanna@gmail.com>
2020-05-18 13:16:25 +0300

    task_1 changed

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -2,24 +2,29 @@ let modalMain = document.getElementById('modal_main');
 let modalSuccess = document.getElementById('modal_success');
 let btnSuccess = modalSuccess.getElementsByClassName('btn_success');
 let modalClose = document.getElementsByClassName('modal__close');
+console.log(modalClose);
 
+//добавляем активный класс модальному окну (чтобы показать его на странице)
 modalMain.classList.add('modal_active');
 
+//при клике на крестик закрываем окно
 modalClose[0].onclick = function () {
     modalMain.classList.remove('modal_active');
 }
 
+//при клике по ссылке (кнопке) активируем второе модальное окно и закрываем первое
 modalClose[1].onclick = function () {
+    modalMain.classList.remove('modal_active');
     modalSuccess.classList.add('modal_active');
+
 }
 
+//при клике по крестику во втором окне закрываем окно
 modalClose[2].onclick = function () {
-
-    modalMain.classList.remove('modal_active');
     modalSuccess.classList.remove('modal_active');
 }
 
+//при клике по кнопке второго окна закрываем окно
 btnSuccess[0].onclick = function () {
-    modalMain.classList.remove('modal_active');
     modalSuccess.classList.remove('modal_active');
 }
