6913a1edc75ed9c08d778166ec1eac461a07451b
Ann Vasilyeva <skazkadreamanna@gmail.com>
2020-05-20 10:37:02 +0300

    task_1

---
+++ dom/reveal/task.js
@@ -0,0 +1,18 @@
+
+document.addEventListener('scroll', function () {
+    let reveals = document.querySelectorAll('.reveal');
+    let viewportHeight = window.innerHeight;
+
+   for (let i = 0; i < reveals.length; i++) {
+       let revealTop = reveals[i].getBoundingClientRect().top;
+       let revealBottom = reveals[i].getBoundingClientRect().bottom;
+
+       if (revealTop < viewportHeight) {
+               reveals[i].classList.add('reveal_active');
+           }
+       if (revealBottom === 0) {
+           reveals[i].classList.remove('reveal_active');
+       }
+   }
+});
+
