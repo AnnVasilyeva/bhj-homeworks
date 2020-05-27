0b2da8f930adae225c6d8c00fad7d0ed56c3d191
Ann Vasilyeva <skazkadreamanna@gmail.com>
2020-05-27 13:40:35 +0300

    task_1

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,22 +1,30 @@
 let hasTooltip = document.querySelectorAll('.has-tooltip');
+let tooltipTarget = null;
 
 for (let i = 0; i < hasTooltip.length; i++) {
     hasTooltip[i].addEventListener('click', (evt) => {
         evt.preventDefault();
-let tooltip = document.querySelector('.tooltip');
-let  hasTooltipPosition = hasTooltip[i].getBoundingClientRect();
+        let tooltip = document.querySelector('.tooltip');
+        //получем все координаты ссылки
+        let  hasTooltipPosition = hasTooltip[i].getBoundingClientRect();
 
-        if (tooltip.style.display === 'block') {
-            tooltip.style.display = 'none';
+        //проверка, если это та ссылка которую нажали
+        if (tooltipTarget === evt.currentTarget) {
+            //если подсказка не видима, сделать ее видимой
+            if (tooltip.style.display !== 'block') {
+                tooltip.style.display = 'block';
+            } else { //если наоборот, то скрыть подсказку
+                tooltip.style.display = 'none';
+            }
+        //иначе присвоить значение кликнутой ссылки и сделать подсказку видимой
         } else {
+            tooltipTarget = evt.currentTarget;
             tooltip.style.display = 'block';
-            tooltip.style.top = hasTooltipPosition.bottom + 'px';
-            tooltip.style.left = hasTooltipPosition.left + 'px';
-            tooltip.textContent = hasTooltip[i].title;
         }
-
-
+//присваиваем координаты и текст для подсказки
+        tooltip.style.top = hasTooltipPosition.bottom + 'px';
+        tooltip.style.left = hasTooltipPosition.left + 'px';
+        tooltip.textContent = hasTooltip[i].title;
     })
 }
-
 
