aaf8e8e920dccb5f4502eae39db8d9767253552c
Ann Vasilyeva <skazkadreamanna@gmail.com>
2020-05-27 14:33:31 +0300

    task_3

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -21,33 +21,26 @@ let productAdd = document.querySelectorAll('.product__add');
 [...productAdd].forEach((productAddBtn) => productAddBtn.addEventListener('click', addToCart));
 
 function addToCart(evt) {
-    let isNew = true; //новый товар (да/нет)
-    let cartProduct = null;//задаем товар без значения
     let btnParent = evt.currentTarget.closest('.product');//родитель кликнутой кнопки добавить
     let productId = btnParent.dataset.id;//id товара
     let productImg = btnParent.querySelector('img').src;//картинка товара
     let quantityValue = btnParent.querySelector('.product__quantity-value').textContent;//количество товара
 
-    //проверяем корзину
-    [...productsList.children].forEach((cartItem) => {
-        if (cartItem.dataset.id === productId) { //если в корзине уже есть товар с таким же id как у нового товара, который хотим добавить
-            isNew = false;//это не новый товар
-            cartProduct = cartItem;//значит новый товар равен тому товару что уже есть в корзине
-        }
-    });
+    //проверяем корзину, есть ли там товар с таким же id
+    let cartProduct = [...productsList.children].find( cartItem => cartItem.dataset.id === productId);
 
-    //если товар новый(его нет в корзине)
-    if (isNew) {
+   //если такой товар есть то он становится cartProduct
+    if (cartProduct) {
+        //меняем количество товара (суммируем то что было с тем что пришло)
+        let quantity = cartProduct.querySelector('.cart__product-count').textContent;
+        cartProduct.querySelector('.cart__product-count').textContent = Number(quantityValue) + Number(quantity);
+    } else {  //если товар новый(его нет в корзине)
         //клонируем карточку товара и добавляем в нее данные из кликнутого товара
         cartProduct = (document.querySelector('template').content).cloneNode(true);
         cartProduct.querySelector('.cart__product').dataset.id = productId;
         cartProduct.querySelector('img').src = productImg;
         cartProduct.querySelector('.cart__product-count').textContent = quantityValue;
         productsList.appendChild(cartProduct); //добавляем товар в корзину
-    } else {
-        //меняем количество товара (суммируем то что было с тем что пришло)
-        let quantity = cartProduct.querySelector('.cart__product-count').textContent;
-        cartProduct.querySelector('.cart__product-count').textContent = Number(quantityValue) + Number(quantity);
     }
 }
 
